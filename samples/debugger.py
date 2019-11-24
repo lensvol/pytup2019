4f5f4747bc11a9362a833e04ab2068d509c02dfb
Kirill Borisov <kirill.borisov@booking.com>
2019-11-24 22:06:00 +0100

    Add samples for removal of debugger calls and fixing 'not .. in ..'.

---
+++ samples/debugger.py
@@ -0,0 +1,14 @@
+import pdb
+
+bar()
+
+pdb.set_trace()
+import ipdb; ipdb.set_trace()
+breakpoint()
+
+a = 42
+
+foo("Hi")
+
+if a == b:
+    pass
---
+++ samples/not_in.py
@@ -0,0 +1,5 @@
+if not a in b and b < 42 and not b in c and b not in c:
+    pass
+
+if a not in b:
+    pass
